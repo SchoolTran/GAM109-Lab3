57e33293bbdc0f0b48ff6f68c2e9ef561fa6f078
SchoolTran <truongnulo1709@gmail.com>
2024-05-20 16:38:40 +0700

    first commit

---
+++ Bai1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // .../ ra thư mục cha
+            // C:\\...\\...\\ đường dẫn tương đối 
+            // example.txt đường dẫn tuyệt đối 
+
+            string path = "example.txt";
+
+            // Tạo một đối tượng FileStream để ghi đối tượng 
+            using(FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                // Tạo một đối tượng StreamWriter để ghi dữ liệu vào FileStream 
+                using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    sw.WriteLine("Username: myUserName");
+                    sw.WriteLine("Password: myPassWord");
+                }
+            }
+
+            // Tạo một đối tượng FileStream để đọc đối tượng 
+            using (FileStream fs = new FileStream(path,FileMode.Open, FileAccess.Read))
+            {
+                // Tạo một đối tượng StreamWriter để đọc dữ liệu từ FileStream 
+                using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+     
+}
+
---
+++ Bai2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Truong";
+            string password = "12345";
+            string time = "15/5/2024";
+
+            // Sử dụng StringWriter để lưu tài khoản và mật khẩu 
+            using (StringWriter sw = new StringWriter()) 
+            {
+                sw.WriteLine("Username: " + username);
+                sw.WriteLine("Password: " +  password);
+                sw.WriteLine("Time: " +  time);
+
+                // Lấy chuỗi từ StringWritrer 
+                string content = sw.ToString();
+
+                // Sử dụng StringReader để đọc tài khoản và mật khẩu 
+                var sr = new StringReader(content);
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
